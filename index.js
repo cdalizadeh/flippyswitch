32e621e75bb8d101f19f583c811496cee0488f3e
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-06-22 21:40:21 -0400

    begin developing server, created .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("wow hello");
+console.log("Node Application Starting");
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('server');
+var port = 3000;
+
+server.listen(port, () => {
+    console.log("Server listening at port " + port);
+});
