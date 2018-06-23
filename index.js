f5d8dca14a783d758fa0e4852e81a47d75152cfa
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-06-22 23:41:10 -0400

    working backend infrastructure

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ server.listen(port, () => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var flip = true;
+
 io.on('connection', (socket) => {
     console.log('wow connection is recieve');
     socket.on('flip', () => {
         console.log('switch is flip');
+        flip = !flip;
+        io.sockets.emit('switchFlipped', flip);
     });
 });
