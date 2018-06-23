9ded9cfa6a717773ce1e5b00bd780f76456104b3
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-06-22 22:23:12 -0400

    fixed var server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Node Application Starting");
 var express = require('express');
 var app = express();
 var path = require('path');
-var server = require('server');
+var server = require('http').createServer(app);
 var port = 3000;
 
 server.listen(port, () => {
