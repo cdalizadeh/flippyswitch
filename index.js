ab06737ec3c73475c515bd57f8568a7ce5e53f07
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-11-12 23:43:58 -0500

    use process.env.port

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ console.log("Node Application Starting");
 
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
 var path = require('path');
-var port = 8080;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 8080;
 
 server.listen(port, () => {
     console.log("Server listening at port " + port);
